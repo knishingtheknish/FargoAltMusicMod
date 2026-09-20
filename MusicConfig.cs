2ab0ff37ae15e23c77a29279d0166f2c06c40957
roo <146498770+Ev11nroo@users.noreply.github.com>
2026-09-19 21:43:45 -0400

    only replace mutant themes on config

--- MusicConfig.cs
+++ MusicConfig.cs
@@ -16,5 +16,8 @@ namespace knishfargomusic
         [DefaultValue(true)]
         [ReloadRequired]
         public bool OverrideModdedMusicBoxes;
+
+        [DefaultValue(true)]
+        public bool MutantFtw;
     }
 }
